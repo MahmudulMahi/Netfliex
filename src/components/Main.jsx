bc1ae8f3910a42a2456c4187119936ee236d56ab
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-03-03 12:49:53 +0600

    add movie randomly

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,10 @@ import requests from '../Requests';
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
-  console.log("mmm", movies)
+ 
 
   const movie = movies[Math.floor(Math.random() * movies.length)];
+  console.log("mmm", movies)
 
   useEffect(()=>{
     axios.get(requests.requestPopular).then((res)=>{
@@ -14,6 +15,7 @@ const Main = () => {
     })
     
   },[])
+  
 
   // useEffect(() => {
   //   axios.get("https://api.themoviedb.org/3/movie/popular?api_key=5cd55e05d4a7c6a604ed113e66a5998a&language=en-US&page=1")
@@ -28,25 +30,25 @@ const Main = () => {
   // }, []);
   //   console.log(movie);
 
-  const truncateString = (str, num) => {
-    if (str?.length > num) {
-      return str.slice(0, num) + '...';
-    } else {
-      return str;
-    }
-  };
+  // const truncateString = (str, num) => {
+  //   if (str?.length > num) {
+  //     return str.slice(0, num) + '...';
+  //   } else {
+  //     return str;
+  //   }
+  // };
 
   return (
     <div className='w-full h-[600px] text-white'>
       <div className='w-full h-full'>
         <div className='absolute w-full h-[600px] bg-gradient-to-r from-black'></div>
-        <img
+        {/* <img
           className='w-full h-full object-cover'
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
-        />
+        /> */}
         <div className='absolute w-full top-[20%] p-4 md:p-8'>
-          <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+          {/* <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1> */}
           <div className='my-4'>
             <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
               Play
@@ -56,11 +58,11 @@ const Main = () => {
             </button>
           </div>
           <p className='text-gray-400 text-sm'>
-            Released: {movie?.release_date}
+            {/* Released: {movie?.release_date} */}
           </p>
-          <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>
+          {/* <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>
             {truncateString(movie?.overview, 150)}
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
