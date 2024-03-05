fe71083e54cda59fe19b1e965910d4fd334fa3f5
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-03-05 14:29:39 +0600

    create signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -31,41 +31,7 @@ const Signup = () => {
           <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
             <div className='max-w-[320px] mx-auto py-16'>
               <h1 className='text-3xl font-bold'>Sign Up</h1>
-              <form
-                onSubmit={handleSubmit}
-                className='w-full flex flex-col py-4'
-              >
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  className='p-3 my-2 bg-gray-700 rouded'
-                  type='email'
-                  placeholder='Email'
-                  autoComplete='email'
-                />
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
-                  className='p-3 my-2 bg-gray-700 rouded'
-                  type='password'
-                  placeholder='Password'
-                  autoComplete='current-password'
-                />
-                <button className='bg-red-600 py-3 my-6 rounded font-bold'>
-                  Sign Up
-                </button>
-                <div className='flex justify-between items-center text-sm text-gray-600'>
-                  <p>
-                    <input className='mr-2' type='checkbox' />
-                    Remember me
-                  </p>
-                  <p>Need Help?</p>
-                </div>
-                <p className='py-8'>
-                  <span className='text-gray-600'>
-                    Already subscribed to Netflix?
-                  </span>{' '}
-                  <Link to='/login'>Sign In</Link>
-                </p>
-              </form>
+           
             </div>
           </div>
         </div>
